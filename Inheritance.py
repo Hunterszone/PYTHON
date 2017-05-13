89213f0350af0f20a27f1664619254b32aa16e2e
Konstantin Drenski <k.drenski91@gmail.com>
2017-05-13 11:07:15 +0300

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,38 @@
+class Person:
+  
+  def __init__(self, text, amount):
+  
+   self.sometext = text
+   self.someamount = amount
+    
+  def Money(self):
+    return self.sometext + self.someamount
+  
+class Frauder(Person):
+  
+  def __init__(self, text, amount, case):
+    
+    Person.__init__(self, text, amount)
+    self.somecase = case
+  
+  def Fraud(self):
+    return self.Money() + self.somecase
+    
+class Cop(Frauder):
+  
+  def __init__(self, text, amount, case, jail):
+    
+    Frauder.__init__(self, text, amount, case)
+    self.somejail = jail
+  
+  def Jail(self):
+    return y.Fraud() + self.somejail
+
+x = Person("I have " , str(123))
+y = Frauder("You have " , str(123), " - got stolen")
+z = Cop("I have " , str(123), " stolen", " => go to jail!")
+
+print("Normal person says: ", x.Money())
+print("Frauder says: ", y.Fraud())
+print("The cop says: ", z.Jail())
+    

ce93ed4969ec4c629d083c6f52f91540f59c87f3
Hunterszone <k.drenski91@gmail.com>
2018-01-21 00:42:20 +0200

    edited

--- Python tasks/Personal_data.py
+++ Python tasks/Personal_data.py
@@ -1,15 +1,17 @@
 name1 = input("first name: ")
 name2 = input("second name: ")
 name3 = input("third name: ")
-Year = input("birth year: ")
+BirthYear = input("birth year: ")
+ThisYear = input("this year: ")
  
 age1 = {}
-age1 = 2015 - 1991
+age1 = int(ThisYear) - int(BirthYear)
  
-person = {"first name":"Ivan",
-          "second name":"Petrov",
-          "third name":"Dimitrov",
-          "birth year":"1991 ",
+person = {"first name": str(name1),
+          "second name":str(name2),
+          "third name":str(name3),
+          "birth year":int(BirthYear),
+          "this year":int(ThisYear),
           "age": str(age1)
           }
  

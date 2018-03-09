a6584ab50c524189fc9e8bdb0792ca6c88287949
Hunterszone <k.drenski91@gmail.com>
2018-03-10 00:47:27 +0100

    Copy folder

---
+++ PythonTasks/1 TO 9 9 TO 1 - Copy.py
@@ -0,0 +1,15 @@
+counter = 1
+arr = []
+
+while counter <= 9:
+    print (counter)
+    arr.append(counter)    
+    counter += 1
+
+print(arr)
+counter = 9
+
+while counter >= 1:
+    print (counter)
+    counter -= 1
+
